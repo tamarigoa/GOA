e8bdd1250cf626bab7876d34890770df8463921b
tamarigoa <dachinaveriani07@gmail.com>
2024-10-27 22:53:27 +0400

    commit

---
+++ day  01/classwork/claswork.py/claswork.py
@@ -0,0 +1,12 @@
+name="tamari"
+surname="naveriani"
+num=12
+num1=10.2
+num2=55.5
+num3=50.9
+print(name)
+print(surname)
+print(num)
+print(num1)
+print(num2)
+print(num3)
---
+++ day 00/homework.py
@@ -0,0 +1,7 @@
+#from turtle import
+
+#we want to make a house
+
+#step 1: draw a square
+
+forward(200)
